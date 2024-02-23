94f1911ccf7620b922349dd7b189f7dda7415358
tintoll <belselios@gmail.com>
2024-02-24 08:24:23 +0900

    예수금 정보가져오기 작성중

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -19,14 +19,18 @@ class Kiwoom(QAxWidget):
         self.get_ocx_instance()
         self.event_slots()
         self.signal_login_commConnect()
-        self.get_account_info()
+        self.get_account_info() # 계좌번호 가져오기
+        self.detail_account_info() # 예수금 가져오기
     def get_ocx_instance(self):
         # 키움 OpenAPI+의 OCX 방식을 사용하기 위해서는 OCX의 인스턴스를 얻어와야 합니다.
         # 윈도우 레지스트리에 등록된 ProgID를 사용하여 인스턴스를 얻어옵니다.
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
 
     def event_slots(self):
+        # 로그인 이벤트 연결
         self.OnEventConnect.connect(self.login_slot)
+        # 예수금 요청 이벤트 연결
+        self.OnReceiveTrData.connect(self.trdata_slot)
 
     def login_slot(self, err_code):
         print(errors(err_code))
@@ -44,3 +48,13 @@ class Kiwoom(QAxWidget):
         account_list = self.dynamicCall("GetLoginInfo(String)", "ACCNO")
         self.account_num = account_list.split(';')[0]
         print("나의 보유 계좌번호 %s" % self.account_num)
+
+    def detail_account_info(self):
+        print("예수금을 요청하는 부분")
+        self.dynamicCall("SetInputValue(String, String)", "계좌번호", self.account_num)
+        self.dynamicCall("SetInputValue(String, String)", "비밀번호", "0000")
+        self.dynamicCall("SetInputValue(String, String)", "비밀번호입력매체구분", "00")
+        self.dynamicCall("SetInputValue(String, String)", "조회구분", "2")
+        self.dynamicCall("CommRqData(String, String, int, String)", "예수금상세현황요청", "opw00001", "0", "2000")
+
+        
