4986a9dc172f6da7388eedd1ed2cfa3995fc1e0c
tintoll <belselios@gmail.com>
2024-02-25 11:15:37 +0900

    계좌평가잔고내역 종목들 전체 가져오기(멀티데이터) 구현

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -9,14 +9,18 @@ class Kiwoom(QAxWidget):
         ## event loop 모음 ##
         self.login_event_loop = None
         self.detail_account_info_event_loop = None
-        self.detail_account_info_event_loop_2 = None
+        self.detail_account_info_event_loop_2 = QEventLoop()
         ####################
 
         ## 변수 모음 ##
         self.account_num = None
+        self.account_stock_dict = {}
         ####################
 
-
+        ## 계좌 관련 변수 ##
+        self.use_money = 0
+        self.use_money_percent = 0.5
+        ####################
 
         self.get_ocx_instance()
         self.event_slots()
@@ -71,8 +75,7 @@ class Kiwoom(QAxWidget):
         self.dynamicCall("SetInputValue(String, String)", "조회구분", "2")
         self.dynamicCall("CommRqData(String, String, int, String)", "계좌평가잔고내역요청", "opw00018", sPrevNext, "2000")
 
-        self.detail_account_info_event_loop_2 = QEventLoop()
-        self.detail_account_info_event_loop_2.exec_()
+        self.detail_account_info_event_loop_2.exec_() # 다시 요청이 오면 중복 경고가 나올수 있으나 큰 문제는 없다.
 
     def trdata_slot(self, sScrNo, sRQName, sTrCode, sRecordName, sPrevNext):
         '''
@@ -89,6 +92,11 @@ class Kiwoom(QAxWidget):
             print("예수금 %s" % deposit) # "000000010000000"
             print("예수금 형변환 %s" % int(deposit)) # 10000000
 
+            # 현재 예수금에서 실제 사용할 돈을 계산
+            self.use_money = int(deposit) * self.use_money_percent
+            # 한번에 사용할 금액을 4등분해서 사용
+            self.use_money = self.use_money / 4
+
             ok_deposit = self.dynamicCall("GetCommData(String, String, int, STring)", sTrCode, sRQName, 0, "출금가능금액")
             print("출금가능금액 %s" % ok_deposit)
             print("출금가능금액 형변환 %s" % int(ok_deposit))
@@ -102,5 +110,50 @@ class Kiwoom(QAxWidget):
             total_profit_loss_rate = self.dynamicCall("GetCommData(String, String, int, STring)", sTrCode, sRQName, 0, "총수익률(%)")
             total_profit_loss_rate_result = float(total_profit_loss_rate)
             print("총수익률(%%) %s" % total_profit_loss_rate_result)
+            
+            # 멀티데이터 조회 (내가 매수한 종목들에 대한 정보를 조회)
+            # QString으로 통일하자, 원래 아무명이나 사용해도 되는데 안되는경우도 있다고 함.
+            rows = self.dynamicCall("GetRepeatCnt(QString, QString)", sTrCode, sRQName)
+            cnt = 0 # 종목이 몇개인지 확인
+            for i in range(rows):
+                # A003322 = 종목번호, A:장내주식,J:ETF,Q:ETN종목
+                code = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "종목번호")
+                code = code.strip()[1:] # 앞에 문자 하나 제거, strip()은 양쪽공백제거
+                code_nm = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "종목명")
+                stock_quantity = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "보유수량")
+                buy_price = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "매입가")
+                learn_rate = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "수익률(%)")
+                current_price = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "현재가")
+                total_chegyul_price = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "매입금액")
+                possible_quantity = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "매매가능수량")
 
-            self.detail_account_info_event_loop_2.exit()
+                # dict에 종목코드가 없으면 추가
+                if code in self.account_stock_dict:
+                    pass
+                else:
+                    self.account_stock_dict.update({code: {}})
+
+                code_nm = code_nm.strip()
+                stock_quantity = int(stock_quantity.strip())
+                buy_price = int(buy_price.strip())
+                learn_rate = float(learn_rate.strip())
+                current_price = int(current_price.strip())
+                total_chegyul_price = int(total_chegyul_price.strip())
+                possible_quantity = int(possible_quantity.strip())
+
+                # dict에 종목코드에 대한 정보를 업데이트
+                self.account_stock_dict[code].update({"종목명": code_nm})
+                self.account_stock_dict[code].update({"보유수량": stock_quantity})
+                self.account_stock_dict[code].update({"매입가": buy_price})
+                self.account_stock_dict[code].update({"수익률": learn_rate})
+                self.account_stock_dict[code].update({"현재가": current_price})
+                self.account_stock_dict[code].update({"매입금액": total_chegyul_price})
+                self.account_stock_dict[code].update({"매매가능수량": possible_quantity})
+                cnt += 1
+            print("계좌에 가지고 있는 종목 %s" % self.account_stock_dict)
+            print("계좌에 가지고 있는 종목 수 %s" % cnt)
+
+            if sPrevNext == "2": # 0이면 다음페이지가 없다는 뜻, 2이면 다음페이지가 있다는 뜻
+                self.detail_account_mystock(sPrevNext="2")
+            else:
+                self.detail_account_info_event_loop_2.exit() # 다음페이지가 없으면 이벤트루프를 종료시킨다.
