ab0d2192cd9afde0d0f329e3c357c8a9f8ecf8a2
tintoll <belselios@gmail.com>
2024-03-05 19:26:56 +0900

    코스피, 코스닥 종목 가져오는 부분 추가

--- stock/OpenApi.py
+++ stock/OpenApi.py
@@ -21,10 +21,20 @@ class OpenApi(QAxWidget):
         self.set_signal_slots()
 
         self.login()
+        # 종목 코드 가져오기
+        all_code_list = self.getStockCodeList() # [000020,00000, ...]
 
 
 
-
+    def getStockCodeList(self):
+        kospi_code_list = self.dynamicCall("GetCodeListByMarket(QString)", "0")
+        kospi_code_list = kospi_code_list.split(';')[:-1]  # 마지막은 공백이라서 제거
+        kosdaq_code_list = self.dynamicCall("GetCodeListByMarket(QString)", "10")
+        kosdaq_code_list = kosdaq_code_list.split(';')[:-1]
+        logger.info("코스피 종목 갯수 : %s" % len(kospi_code_list))
+        logger.info("코스닥 종목 갯수 : %s" % len(kosdaq_code_list))
+        all_code_list = kospi_code_list + kosdaq_code_list
+        return all_code_list
 
     def set_signal_slots(self):
         self.OnEventConnect.connect(self.login_slot)
